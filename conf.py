d64ea166e44440129f806cac49e1f1d29fdd4cd3
laalaguer <laalaguer@gmail.com>
2019-09-02 13:21:29 +0800

    1.0.0

--- conf.py
+++ conf.py
@@ -22,7 +22,7 @@ copyright = u'2019, laalaguer'
 author = 'laalaguer'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+release = '1.0.0'
 
 # -- Backward compatibility with Sphinx v1.8.3 -------------------------------
 
